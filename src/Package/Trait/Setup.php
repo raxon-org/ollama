08276de09f8232dedb295e77986e110db1fc8eff
Remco van der Velde <remco@universeorange.com>
2026-02-26 18:06:27 +0100

    init

--- src/Package/Trait/Setup.php
+++ src/Package/Trait/Setup.php
@@ -1,6 +1,7 @@
 <?php
 namespace Package\Raxon\Ollama\Trait;
 
+use Raxon\Module\Core;
 use Raxon\Module\Dir;
 
 trait Setup {
@@ -8,6 +9,7 @@ trait Setup {
     public function install_ollama(string $command = ''): void
     {
         if(!Dir::exist('/root/.ollama/')){
+            Core::interactive();
             $command = 'curl -fsSL https://ollama.com/install.sh | sh';
             exec($command, $output);
             echo implode(PHP_EOL, $output);
