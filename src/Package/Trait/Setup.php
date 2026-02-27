457792e0c73458512aed9de34e7c83fb42b81788
Remco van der Velde <remco@universeorange.com>
2026-02-27 19:13:18 +0100

    init

--- src/Package/Trait/Setup.php
+++ src/Package/Trait/Setup.php
@@ -6,13 +6,21 @@ use Raxon\Module\Dir;
 
 trait Setup {
 
-    public function install_ollama(string $command = ''): void
+    public function install_ollama(object $flags, object $options): void
     {
         if(!Dir::exist('/root/.ollama/')){
             Core::interactive();
             $command = 'curl -fsSL https://ollama.com/install.sh | sh';
             exec($command, $output);
             echo implode(PHP_EOL, $output);
+            $this->pull_model($flags, $options);
         }
+    }
+
+    public function pull_model(object $flags, object $options): void
+    {
+        $command = 'ollama pull qwen3-embedding';
+        exec($command, $output);
+        echo implode(PHP_EOL, $output);
     }
 }
