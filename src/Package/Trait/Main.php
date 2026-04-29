57ec5ab87fc220279c30f9847c45eed898497f19
Remco van der Velde <remco@universeorange.com>
2026-04-29 11:41:25 +0200

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -407,10 +407,12 @@ trait Main {
                                     'updated' => date('Y-m-d H:i:s')
                                 ]
                             ];
+                            d($patch);
                             $node = new Node($object);
                             $class = 'Raxon.Ollama.Input';
                             $role = $node->role_system();
                             $patch = $node->patch($class, $role, $patch);
+                            ddd($patch);
                             exit(0);
                         }
                         $time_current = microtime(true);
